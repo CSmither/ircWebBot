c3f316f03daaa5d1c872fab127e9ad0c94863b55
CSmither <callum@smither.org>
2019-02-10 00:08:20 +0000

    Reattached workspace added rollbar

--- src/main/java/org/smither/ircWebBot/Application.java
+++ src/main/java/org/smither/ircWebBot/Application.java
@@ -1,11 +1,19 @@
 package org.smither.ircWebBot;
 
+import com.rollbar.notifier.Rollbar;
+import com.rollbar.notifier.config.Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import static com.rollbar.notifier.config.ConfigBuilder.withAccessToken;
+
 @SpringBootApplication
-public class Application  {
+public class Application {
+	private static Rollbar rollbar;
+
 	public static void main(String[] args) {
+		Config config = withAccessToken("f42e52144a9d4159815aeb48bb6ba363").environment("production").codeVersion("1.0.0").build();
+		rollbar = Rollbar.init(config);
 		SpringApplication.run(Application.class, args);
 	}
 }
