ed234b306c612c97d764f30f9265270846f1912e
CSmither <callum@smither.org>
2019-02-12 22:48:23 +0000

    Maybe?

--- src/main/java/org/smither/ircWebBot/ExceptionController.java
+++ src/main/java/org/smither/ircWebBot/ExceptionController.java
@@ -3,28 +3,37 @@ package org.smither.ircWebBot;
 import com.rollbar.notifier.Rollbar;
 import com.rollbar.web.provider.RequestProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.info.BuildProperties;
-import org.springframework.stereotype.Service;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.SimpleMappingExceptionResolver;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Properties;
 
 import static com.rollbar.notifier.config.ConfigBuilder.withAccessToken;
 
-@Service
 public class ExceptionController extends SimpleMappingExceptionResolver {
 
+	private String defaultErrorView = "error";
+	private String exceptionAttribute = "ex";
 	private String accessToken = "f42e52144a9d4159815aeb48bb6ba363";
-
 	private Rollbar rollbar;
-
 	@Autowired
 	private BuildProperties buildProperties;
 
 	public ExceptionController() {
 		setWarnLogCategory(ExceptionController.class.getName());
+	}
+
+	@Bean
+	@Qualifier("exceptionMappings")
+	public Properties getExceptionMappings() {
+		Properties props = new Properties();
+		props.setProperty("java.lang.Exception", "error");
+		return props;
 	}
 
 	@Override
