aadaf6a741f2ece744d659bb3eab5f0576507b23
smither <syessriwwgMa9m!>
2019-02-13 13:43:30 +0000

    Release 1.2.0

--- src/main/java/org/smither/ircWebBot/Application.java
+++ src/main/java/org/smither/ircWebBot/Application.java
@@ -1,7 +1,5 @@
 package org.smither.ircWebBot;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
